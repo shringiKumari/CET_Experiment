81e3f692fc978bfd86aa40575f416bb0088e43f3
Shringi <sk1382@york.ac.uk>
2017-02-22 13:37:42 +0000

    Add files that represent structure for storing player data

---
+++ Assets/Scripts/AttemptModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Attempts {
+
+     [System.Serializable]
+     public class AttemptModel {
+          public float timeToReachGoal;
+          public float timeOfDeath;
+          public int deathOnPlatform; 
+     }
+     public AttemptModel[] attempts;
+}
---
+++ Assets/Scripts/AttemptStore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttemptStore : MonoBehaviour {
+
+     public void OnAttempt(float timeToReachGoal, float timeOfDeath, int deathOnPlatform)
+     {
+          
+     }
+
+     // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
