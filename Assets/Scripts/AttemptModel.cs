581c4f24a6e36890677fdd985c9c142bb9418e8f
Shringi <sk1382@york.ac.uk>
2017-02-22 20:11:50 +0000

    created model, file and data serialisation

---
+++ Assets/Scripts/AttemptModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Attempts {
+
+     [System.Serializable]
+     public struct AttemptModel {
+          public float timeToReachGoal;
+          public float timeOfDeath;
+          public int deathOnPlatform;
+
+          public AttemptModel(float timeToReachGoal, float timeOfDeath, int deathOnPlatform) {
+               this.timeToReachGoal = timeToReachGoal;
+               this.timeOfDeath = timeOfDeath;
+               this.deathOnPlatform = deathOnPlatform;
+          }
+     }
+     public List<AttemptModel> attempts;
+
+     public void Add(AttemptModel model) {
+          if (attempts == null) {
+               attempts = new List<AttemptModel> ();
+          }
+               
+          attempts.Add (model);
+     }
+}
---
+++ Assets/Scripts/StoreAttempt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+public class StoreAttempt : MonoBehaviour {
+
+     public void OnAttempt(float timeToReachGoal, float timeOfDeath, int deathOnPlatform) {
+          Attempts.AttemptModel attempt = new Attempts.AttemptModel (timeToReachGoal, timeOfDeath, deathOnPlatform);
+
+          FileStream stream = new FileStream (Application.persistentDataPath + "/attempts.json", FileMode.OpenOrCreate);
+          StreamReader reader = new StreamReader (stream);
+          string json = reader.ReadToEnd ();
+          Debug.Log (json.Length);
+          stream.Close ();
+          stream = new FileStream (Application.persistentDataPath + "/attempts.json", FileMode.Truncate);
+
+          Attempts attempts = new Attempts ();
+          if (!string.IsNullOrEmpty (json)) {
+               attempts = JsonUtility.FromJson<Attempts> (json);
+          }
+
+          attempts.Add (attempt);
+          json = JsonUtility.ToJson(attempts);
+          StreamWriter writer = new StreamWriter (stream);
+          writer.Write (json);
+          writer.Flush ();
+          stream.Close ();
+     }
+}
