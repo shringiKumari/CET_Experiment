fd32ac11e3750c97544b1789352fac55fdcc3767
Shringi <sk1382@york.ac.uk>
2017-03-12 17:25:20 +0000

    time hud added

---
+++ Assets/TimeHUD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeHUD : MonoBehaviour {
+
+     public Text timeText;
+     private float timeFromStart;
+     private int timeInMin;
+     private float timeInSec;
+     private string timeGap;
+
+     // Use this for initialization
+	void Start () {
+		
+          timeFromStart = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          timeFromStart += Time.deltaTime;
+          timeInMin = Mathf.FloorToInt(timeFromStart / 60);
+          timeInSec = Mathf.FloorToInt(timeFromStart % 60);
+
+          if (timeInSec < 10) {
+               timeGap = "0";
+          } else {
+               timeGap = "";
+          }
+
+          timeText.text = " Time " + timeInMin.ToString() + ":" + timeGap + timeInSec.ToString();
+
+		
+	}
+}
