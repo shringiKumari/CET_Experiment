0e6c76866d36ef4f8fd357a00a9c2fa7d3b5e00f
Shringi <sk1382@york.ac.uk>
2017-03-18 22:58:45 +0000

    added fonts and new game start

---
+++ Assets/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+     [SerializeField] private GameObject gameStartScreen;
+     // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void Onclick () {
+          
+          gameStartScreen.SetActive(false);
+     }
+
+}
