c3473353b04fbe368c52b9d06c9209374b7696b3
Shringi <sk1382@york.ac.uk>
2017-03-28 14:37:05 +0100

    not enough coins popup added

---
+++ Assets/NotEnoughCoinsOkButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotEnoughCoinsOkButtonClick : MonoBehaviour {
+
+     public GameObject notEnoughCoinsPopup;
+     // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void OnClick() {
+          notEnoughCoinsPopup.SetActive (false);
+     }
+}
---
+++ Assets/UpgradeButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+     public GameObject notEnoughCoinsPopup;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void OnClick(){
+          notEnoughCoinsPopup.SetActive (true);
+     }
+}
