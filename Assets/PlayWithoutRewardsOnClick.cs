fe43fed46c5593799a8cf7663d55ab27f3915bb2
Shringi <sk1382@york.ac.uk>
2017-03-30 14:31:29 +0100

    play button no reward added

---
+++ Assets/PlayWithoutRewardsOnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWithoutRewardsOnClick : MonoBehaviour {
+
+	// Use this for initialization
+     public Pauser pause;
+     public GameObject noRewardsPopup;
+     public GameObject levelStart;
+
+
+     void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void OnClick() {
+          pause.Pause (false);
+          levelStart.SetActive (false);
+          noRewardsPopup.SetActive (false);
+
+     }
+}
