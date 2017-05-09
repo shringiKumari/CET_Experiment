07ece79866d424a4d5ca76fbe2572985fc5724a0
Shringi <sk1382@york.ac.uk>
2017-05-09 18:21:23 +0100

    instruction text added

---
+++ Assets/InstructionText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionText : MonoBehaviour {
+
+     public Text instructionText;
+
+     private GlobalData globalData;
+
+     // Use this for initialization
+	void Start () {
+
+          /*globalData = GameObject.FindGameObjectWithTag("GlobalData").GetComponent<GlobalData>();
+          if (globalData.coins_condition) {
+               instructionText.text = "abcdefgh";
+          } else {
+               instructionText.text = "abcd";
+          }
+          */
+
+          instructionText.text = "abcdefgh";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
