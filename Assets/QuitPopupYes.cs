b438b9de9e105573a314c873b3a6190a1784fcc5
Shringi <sk1382@york.ac.uk>
2017-05-10 15:13:41 +0100

    quit yes/no works on level end screen

---
+++ Assets/QuitPopupYes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class QuitPopupYes : MonoBehaviour {
+     [SerializeField] private Pauser pauser;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void OnClick() {
+
+          pauser.Pause (false);
+          SceneManager.LoadScene("Questions_End", LoadSceneMode.Single);
+
+          //Debug.Log ("Application Quit");
+          //Application.Quit ();
+     }
+}
